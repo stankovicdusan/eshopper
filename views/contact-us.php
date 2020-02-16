70316cbe766e9a8f4e0b0f3d0ac42269f13522c6
Dusan <49788140+stankovicdusan@users.noreply.github.com>
2020-02-17 00:54:52 +0100

    Update contact-us.php

--- views/contact-us.php
+++ views/contact-us.php
@@ -3,8 +3,6 @@
         <div class="row">
             <div class="col-sm-12">
                 <h2 class="title text-center">Contact <strong>Us</strong></h2>
-                <div id="gmap" class="contact-map">
-                </div>
             </div>
         </div>
         <div class="row">
