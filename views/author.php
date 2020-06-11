776d458c94b974544e82f60ddf96b729337b15e3
Dusan <49788140+stankovicdusan@users.noreply.github.com>
2020-06-11 17:55:35 +0200

    Add files via upload
    added author page

---
+++ views/author.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once "config/connection.php";
+zabeleziPristupStranici();
+
+?>
+<div class="container">
+	<div class="row">
+		<div class="col-lg-6">
+			<img src="assets/images/profile-photo.jpg" alt="Profile photo">
+		</div>
+		<div class="col-lg-6">
+			<h3 class="about-me">About me</h3>
+			<p><strong>Name:</strong> Dušan Stanković</p>
+			<p><strong>Date of birth:</strong> 25.08.1997.</p>
+			<p><strong>Email:</strong> stankovic.dusan4@gmail.com</p>
+			<p><strong>Github link:</strong> <a href="https://github.com/stankovicdusan">https://github.com/stankovicdusan</a></p>
+			<p><strong>Job:</strong> PHP/Wordpress @ IT-Lion</p>
+		</div>
+	</div>
+</div>
